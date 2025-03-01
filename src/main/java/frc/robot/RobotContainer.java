c0c5c9c7691d76cb3c449cee882d49f3a6515b94
Drive Computer <camp4101@gmail.com>
2025-03-01 11:50:22 -0500

    Lift mods

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,13 +33,13 @@ public class RobotContainer {
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
+    private final CommandXboxController gamepad1 = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
     public LiftSubsystem lift_subsystem = new LiftSubsystem();
 
-    private final double liftpow = .10;
+    private final double liftpow = .75;
     private final Command c_lift_up = new InstantCommand( () -> lift_subsystem.setPower(liftpow) );
     private final Command c_lift_down = new InstantCommand( () -> lift_subsystem.setPower(-liftpow) );
     private final Command c_lift_off = new InstantCommand( () -> lift_subsystem.setPower(0) );
@@ -55,33 +55,33 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX(-gamepad1.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(-gamepad1.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-gamepad1.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
 
-        //lift_subsystem.setDefaultCommand(lift_subsystem.setLiftPower(joystick.getRightTriggerAxis()-joystick.getLeftTriggerAxis()) );
+        //lift_subsystem.setDefaultCommand(lift_subsystem.setLiftPower(gamepad1.getRightTriggerAxis()-gamepad1.getLeftTriggerAxis()) );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        gamepad1.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        gamepad1.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-gamepad1.getLeftY(), -gamepad1.getLeftX()))
         ));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        gamepad1.back().and(gamepad1.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        gamepad1.back().and(gamepad1.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        gamepad1.start().and(gamepad1.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        gamepad1.start().and(gamepad1.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // reset the field-centric heading on left bumper press
-        //joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
-        joystick.rightBumper().onTrue(c_lift_up).onFalse(c_lift_off);
-        joystick.leftBumper().onTrue(c_lift_down).onFalse(c_lift_off);
+        gamepad1.povUp().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        gamepad1.rightBumper().onTrue(c_lift_up).onFalse(c_lift_off);
+        gamepad1.leftBumper().onTrue(c_lift_down).onFalse(c_lift_off);
 
         drivetrain.registerTelemetry(logger::telemeterize);
-        //joystick.rightBumper().whileTrue(lift_subsystem.setLiftPower(1), lift_subsystem.setLiftPower(0));
+        //gamepad1.rightBumper().whileTrue(lift_subsystem.setLiftPower(1), lift_subsystem.setLiftPower(0));
     }
 
     public Command getAutonomousCommand() {
--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -5,39 +5,91 @@
 package frc.robot.subsystems;
 
 import java.util.function.Supplier;
+import static edu.wpi.first.units.Units.*;
 
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.units.measure.Voltage;
 import frc.robot.Constants.LiftConstants;
 
 public class LiftSubsystem extends SubsystemBase {
     TalonFX m_lift_motor1 = new TalonFX(LiftConstants.lift_motor1_id);
     TalonFX m_lift_motor2 = new TalonFX(LiftConstants.lift_motor2_id);
+
+  private final ElevatorFeedforward elevFF = new ElevatorFeedforward(
+    0,0,0,0
+  );
+
+  /* SysId routine for characterizing translation. This is used to find PID gains for the drive motors. */
+    private final SysIdRoutine m_sysIdRoutineLift = new SysIdRoutine(
+        new SysIdRoutine.Config(
+            null,        // Use default ramp rate (1 V/s)
+            Volts.of(4), // Reduce dynamic step voltage to 4 V to prevent brownout
+            null,        // Use default timeout (10 s)
+            // Log state with SignalLogger class
+            state -> SignalLogger.writeString("SysIdTranslation_State", state.toString())
+        ),
+        new SysIdRoutine.Mechanism(
+            output -> setVoltage(output),
+            null,
+            this
+        )
+    );
+
   /** Creates a new LiftSubsystem. */
   public LiftSubsystem() {
-    //TalonFXConfiguration lift_motor_config1 = new TalonFXConfiguration();
-    MotorOutputConfigs lift_motor_output_config = new MotorOutputConfigs().withInverted(InvertedValue.CounterClockwise_Positive);
+    TalonFXConfiguration lift_motor_config = new TalonFXConfiguration();
+    //Set lift motor pid values
+    lift_motor_config.Slot0.kP = 1;
+    lift_motor_config.Slot0.kI = 0;
+    lift_motor_config.Slot0.kD = 0;
+
+    //MotorOutputConfigs lift_motor_output_config = new MotorOutputConfigs().withInverted(InvertedValue.CounterClockwise_Positive);
     //lift_motor_config1.withMotorOutput(lift_motor_output_config);
-    m_lift_motor1.getConfigurator().apply(lift_motor_output_config);
+    
+    m_lift_motor1.getConfigurator().apply(lift_motor_config);
+    m_lift_motor2.getConfigurator().apply(lift_motor_config);
+    m_lift_motor2.setControl(new Follower(LiftConstants.lift_motor1_id, true));
+
   }
 
   public void setPower(double liftPower)
   {
     m_lift_motor1.set(liftPower);
-    m_lift_motor2.set(liftPower);
+    //m_lift_motor2.set(liftPower);
+  }
+
+  public void setVoltage(Voltage inputVoltage)
+  {
+    m_lift_motor1.setVoltage(inputVoltage.magnitude());
+    //m_lift_motor2.setVoltage(inputVoltage.magnitude());
   }
 
   public void turnOff()
   {
     m_lift_motor1.set(0);
     m_lift_motor2.set(0);
+  }
+
+  public double getPos()
+  {
+    return m_lift_motor1.getPosition(true).getValueAsDouble();
+  }
+
+  public void zeroLiftPos()
+  {
+    m_lift_motor1.setPosition(0);
   }
   /**
    * Example command factory method.
@@ -70,6 +122,7 @@ public class LiftSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Lift Position", getPos());
   }
 
   @Override
